5efbee061a1d7a9b5599dfac2085d0af64aa600b
Anand-creator-cyber <60336449+Anand-creator-cyber@users.noreply.github.com>
2020-03-03 21:34:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,31 +3,33 @@ var position;
 var database;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
     var ballPosition = database.ref('ball/position');
     ballPosition.on("value",readPosition, showError);
 
-    database = firebase.database();
-
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(1,0);
+    if(position != defined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+        drawSprites();
     }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
+
     
 }
 
@@ -38,8 +40,8 @@ function readPosition(data){
 }
 
 function writePosition(x,y){
-    Database.ref('ball/position').set({ 
-        'x': position.x + x , 
+    database.ref('ball/position').set({ 
+        'x': position.x + x, 
         'y': position.y + y    })
             
          
@@ -47,5 +49,6 @@ function writePosition(x,y){
 function showError(){
     console.log("Hello");
 }
+
 
 
